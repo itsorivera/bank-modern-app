1a7b682157fdfc898de760758cd9a7d01f1e5209
itsorivera <its.orivera@gmail.com>
2022-12-29 18:07:44 -0500

    Hero and Stats

--- src/components/GestStarted.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GestStarted = () => {
-  return (
-    <div>GestStarted</div>
-  )
-}
-
-export default GestStarted
--- src/components/index.js
+++ src/components/index.js
@@ -9,7 +9,7 @@ import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import Button from './Button'
-import GetStarted from './GestStarted'
+import GetStarted from './GetStarted'
 import FeedbackCard from './FeedbackCard'
 
 export {
